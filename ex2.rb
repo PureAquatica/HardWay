9d9c6b47e4d612786ab24b18347a8165d5319b7e
pureaquatica <mat.duck@gmail.com>
2015-11-01 18:53:51 +0000

    Second exercsie

---
+++ ex2.rb
@@ -0,0 +1,9 @@
+# A cinnebtm this is so you can read your program later.
+# Anything after the # is ignored. 
+
+puts "I could have code like this" # and the comment after is ignored.
+
+# You can alos use a comment to "disable" a comment or piece of code:
+# puts "This wont run"
+
+puts "This will run."

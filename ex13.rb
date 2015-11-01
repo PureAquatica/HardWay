1b3c25dd6ab05a15a57e904868111522b22b9396
pureaquatica <mat.duck@gmail.com>
2015-11-01 21:25:37 +0000

    Excercise 16 final go

---
+++ ex13.rb
@@ -0,0 +1,8 @@
+first, second, third = ARGV
+
+puts "Your first variable is: #{first}"
+puts "Your second variable is: #{second}"
+puts "Your third variable is: #{third}"
+
+# Gets chomp works the same way, except it asks the user to enter information instaed
+# of taking command line arguments

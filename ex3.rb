9121ffa95f8a9eec8c41be2c607cc27b678f3ae8
pureaquatica <mat.duck@gmail.com>
2015-11-01 19:06:12 +0000

    Third excercise

---
+++ ex3.rb
@@ -0,0 +1,24 @@
+puts "I will now count my chickens"
+
+puts "Hens #{25 + 30 / 6}"
+puts "Roosters #{100 - 25 * 3 % 4}"
+
+puts "Now I will count the eggs:"
+
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+puts 3 + 2 < 5 - 7
+# I have noticed you cannot put the operator symbol ontop of the number that you are wanting to the operation on. + seems to work, but minus does not.
+
+puts "What is 3 + 2? #{ 3 + 2}"
+puts "What is 5 - 7? #{ 5 - 7}"
+
+puts "Oh, that's why its false."
+
+puts "How about some more."
+
+puts "Is it greater? #{ 5 > -2}"
+puts "is it greater or equal? #{5 >= -2}"
+puts "Is it less or equal? #{5 <= -2}"

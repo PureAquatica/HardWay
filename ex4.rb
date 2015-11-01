1005578a4bc101412c7d3cfde11f0b55129d3c64
pureaquatica <mat.duck@gmail.com>
2015-11-01 19:22:27 +0000

    Excercise 4

---
+++ ex4.rb
@@ -0,0 +1,18 @@
+cars = 100
+space_in_a_car = 4.0
+drivers = 30
+passengers = 90
+cars_not_driven = cars - drivers
+cars_driven = drivers
+carpool_capacity = cars_driven * space_in_a_car
+average_passengers_per_car = passengers / cars_driven  
+
+puts "There are #{cars} cars available."
+puts "There are only #{drivers} drivers available"
+puts "There will be #{cars_not_driven} empty cars today."
+puts "We can transport #{carpool_capacity} people today."
+puts "We have #{passengers} to carpool today."
+puts "We need to put about #{average_passengers_per_car} in each car."
+
+# His error comes from not defining something in his carpool capacity line. He probably had a typo.
+# If you dont use a .0 in space in a car, you can only have whole numbers. 

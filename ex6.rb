61ef279c987e2894b297f3aae206b9241b972dad
pureaquatica <mat.duck@gmail.com>
2015-11-01 20:09:51 +0000

    Excercise 6

--- ex6.rb
+++ ex6.rb
@@ -1,12 +1,17 @@
 types_of_people = 10
+
 x = "There are #{types_of_people} types of people"
 binary = "binary"
 do_not = "don't"
+# String in string
 y = "Those who know #{binary} and those who #{do_not}"
 
 puts x
 puts y
+
+# String in string
 puts "I said: #{x}."
+# String in string
 puts "I also said: '#{y}'."
 
 hilarious = false
@@ -17,5 +22,6 @@ puts joke_evaluation
 w= "This is the left side of..."
 e= "a string with a right side."
 
+# Concatinating strings. 
 puts w+e
 

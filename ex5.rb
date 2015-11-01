ff5e802e4fa64f259c62d528b0007c5fe178474a
pureaquatica <mat.duck@gmail.com>
2015-11-01 19:34:26 +0000

    Excercise 5

--- ex5.rb
+++ ex5.rb
@@ -1,14 +1,14 @@
 name = 'Zed A. Shaw'
 age = 35
 height = 74
-weight = 180
+weight = 180 * 0.453592
 eyes = 'Blue'
 teeth = 'White'
 hair = 'Brown'
 
 puts "Let's talk about #{name}"
 puts "He's #{height} inches tall."
-puts "He's #{weight} pounds heavy."
+puts "He's #{weight} kilos heavy."
 puts "Actually that's not too heavy."
 puts "He's got #{eyes} eyes and #{hair} hair."
 puts "His teeth are usually #{teeth} depending on the coffee."
